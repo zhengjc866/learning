fff80c096f34904190d01f310fd353325409b5c8
zhengjc866 <70101129+zhengjc866@users.noreply.github.com>
2026-09-14 09:37:35 +0000

    14

---
+++ 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int prime(int n){
+    if(n<=1) return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    };
+    return 1;
+}
+
+int main(){
+    for(int i=100;i<=200;i++){
+       if(prime(i)){
+           printf("%d ",i);
+       };
+    };
+}
